1321b78af054b3cddc4c0690bf83d14d26e21c18
squidawg <alex.kronin@me.com>
2022-06-16 13:49:09 +0400

    feat: add header and footer

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const WRAPPER = document.querySelector('.wrapper');
+const BODY = document.querySelector('body');
+
+const HEADER = createElement('header', "header");
+
+
+const FOOTER = createElement('footer', 'footer');
 
 
 const SHELF = createElement('div', 'shelf' , 'inner_wrapper');
@@ -11,17 +17,28 @@ const CART_INNER = createElement('div', "cart_inner");
 const CART_TITLE = createElement('div','cart_wrapper');
 
 
+
 const cart_title = createElement('h2', 'cart_title');
 cart_title.innerText = 'Cart';
 
 
 const BORDER_BOX = createElement('div', "border_box");
+const border_inner = createElement('div', "border_inner");
+
+
+const border_title = createElement('h2',"border_title" );
+border_title.innerText = "Drop here";
+
+
 const drag = createElement('span', 'drag-n-drop');
-BORDER_BOX.append(drag)
+
+border_inner.append(border_title, drag)
+BORDER_BOX.append(border_inner)
 CART_TITLE.append(cart_title);
 CART_INNER.append(BORDER_BOX);
 CART.append(CART_TITLE, CART_INNER);
 WRAPPER.append(SHELF, CART);
+BODY.append(HEADER, WRAPPER, FOOTER)
 let books;
 
 
@@ -31,6 +48,36 @@ async function readJSON() {
     books = json;
 }
 
+function footer(){
+    const footer_inner = createElement('div', 'footer_inner');
+    const footer_link = createElement('a', 'footer_link');
+    footer_link.href = 'https://github.com/squidawg'
+    const footer_git = createElement('span', 'footer_git');
+    const footer_year = createElement('h2', 'footer_year');
+    footer_link.append(footer_git);
+    footer_year.innerText = '2022';
+    footer_inner.append(footer_link, footer_year,);
+    FOOTER.append(footer_inner);
+
+}
+function header(){
+    const header_promo = createElement('img', "header_promo");
+    const header_wrapper = createElement('div' ,'header_wrapper');
+    const header_title = createElement('h1', 'header_title');
+    const header_description = createElement('p', 'header_description');
+    const  header_inner = createElement('div', 'wrapper');
+    const button_promo = createElement('button', 'button_promo');
+    button_promo.innerText = "To catalog"
+
+    header_title.innerText = 'Book shop'
+    header_description.innerText = 'Connecting readers with independent booksellers all over the world.'
+    header_promo.src = 'icons/Group 1916.png';
+
+    header_wrapper.append(header_title, header_description, button_promo);
+    header_inner.append(header_promo, header_wrapper);
+    HEADER.append(header_inner);
+
+}
 
 function createElement(tag, ...classList) {
     const element = document.createElement(tag);
@@ -81,6 +128,7 @@ const createCardTemplate = (name, link, book_name, book_price, book_description)
 
     const expand = createElement('button', "button_expand");
 
+
     buttons.append(expand, btn);
     card_inner.append(title, author, price);
     card_inner_wrapper.append(img, card_inner);
@@ -94,6 +142,8 @@ const createCardTemplate = (name, link, book_name, book_price, book_description)
 async function init(){
     await readJSON();
     generateCardAppend();
+    header();
+    footer();
 }
 
 
